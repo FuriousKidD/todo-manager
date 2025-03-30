d07cab238c6beeaa1e6fd4a25feb62f0f8d2f84d
FuRiousKidD <manhematakudzwa@gmail.com>
2025-03-30 03:44:09 +0200

    Declaration of the taskManager header file with all the functions to be used, declared adn documented

--- include/TaskManager.h
+++ include/TaskManager.h
@@ -6,3 +6,54 @@
  * -list of tasks
  * -functions
  */
+#ifndef TASKMANAGER_H
+#define TASKMANAGER_H
+#include <vector>
+#include <Task.h>
+
+/**
+ * @class TaskManager
+ * @brief This class will be used as an object for managing multiple tasks
+ * by keeping track of each task's progress and has functions for adding, removing 
+ * and updating tasks
+ */
+class TaskManager{
+    private:
+        std::vector<Task> tasks; ///< This will be a list of tasks
+
+    public:
+        /**
+         * @brief this function adds a new task to the container of classes. 
+         * It appends a task at the back of the container
+         * @param taskTitle takes the name of the task to be completed
+         */
+        void addTask(const std::string& taskTitle);
+
+        /**
+         * @brief Displays the list of tasks
+         */
+        void displayTasks();
+
+        /**
+         * @brief Marks a task as completed based on its index
+         * @param index takes the index of the task to be marked as complete
+         */
+        void markTaskComplete(int index);
+
+        /**
+         * @brief Removes a task from the list at some position
+         * @param index
+         * @return reomoved task
+         */
+        Task removeTask(int index);
+
+        /**
+         * @brief Gets the number of tasks that are in the list
+         * @return number of lists
+         */
+        int getTaskCount() const;
+
+
+};
+
+#endif //TASKMANAGER_H
