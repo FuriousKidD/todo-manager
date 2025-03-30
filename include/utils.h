81881a52536106d8caf3b3c59ce3e37b9e75dcb9
FuRiousKidD <manhematakudzwa@gmail.com>
2025-03-30 19:13:48 +0200

    Adding the initial declaration of the Utility class which which will contain various utility functions

---
+++ include/utils.h
@@ -0,0 +1,18 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/**
+ * @brief This class is for utility functions which will be used generally by the 
+ * main function for certain repeated tasks.
+ */
+class Utility{
+
+    public:
+        /**
+         * @brief this function clears the terminal(screen) every any time it is called
+         */
+        void clearScreen();
+
+};
+
+ #endif //UTILS_H
