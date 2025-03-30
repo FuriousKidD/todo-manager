09020946578751fe15c09bb772a6448ade8b274c
FuRiousKidD <manhematakudzwa@gmail.com>
2025-03-30 18:17:29 +0200

    Testing the functionality of the implemented classes and functions

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,86 @@
  * -loop to handle user input
  */
 #include <iostream>
-#include "include/Task.h" //if header files belong to different directories then include the path
+#include "../include/TaskManager.h" //if header files belong to different directories then include the path
 
 int main(){
 
+    //Testing TaskManager
+    
+    TaskManager taskManager;
+    int option;
+    std::string taskName;
+    int index;
+
+    do{
+        std::cout << "TO DO LIST MANAGER \n"
+                  << "1. Add Task\n"
+                  << "2. View Tasks\n"
+                  << "3. Mark a Task Complete\n "
+                  << "4. Remove a Task\n"
+                  << "5. Exit\n";
+        std::cout << "Enter option: ";
+
+        std::cin >> option;
+        std::cin.ignore(); //ignores any leftover newline characters in the stream
+
+        switch (option){
+            case 1:
+                std::cout << "Enter task name: ";
+                std::getline(std::cin, taskName);//takes user input including spaces and newline characters
+                taskManager.addTask(taskName);
+                break;
+
+            case 2:
+                taskManager.displayTasks();//displays the current list of tasks
+                break;
+
+            case 3:
+                std::cout << "Enter task index to mark complete: ";
+                std::cin >> index;
+                try{
+                    taskManager.markTaskComplete(index);
+
+                } catch(const std::out_of_range& e){
+                    std::cerr << e.what() << std::endl;
+                }
+                break;
+
+            case 4:
+                std::cout << "Enter task index to mark complete: ";
+                std::cin >> index;
+                try{
+                    std::cout << "Removed task was: " + taskManager.removeTask(index) << std::endl;
+
+                } catch(const std::out_of_range& e){
+                    std::cerr << e.what() << std::endl;
+                }
+                break;
+
+            case 5:
+                std::cout << "Exiting the program...\n";
+                break;
+            default:
+                std::cerr << "INVALID INPUT! TRY AGAIN. \n";
+        }
+    } while(option != 5);
+
+
+    //Testing functionality of Task class
+    {
+        //Creating the task
+        Task gitTut = Task("Finish Git tutorial");
+
+        //Display the task before completion
+        std::cout << "Before Completion:" << std::endl;
+        gitTut.displayTask();
+
+        //Mark as complete
+        gitTut.markComplete();
+
+        //Display the task after completion
+        std::cout << "After Completion:" << std::endl;
+        gitTut.displayTask();
+    }
     return 0;
 }
